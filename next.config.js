e12e4ec77b915c98bd5234ebf27a2cead2928ddc
andhim <anditoinfo@gmail.com>
2025-05-26 01:24:01 +0700

    menambahkan file next.config.js

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/sitemap.xml',
+      },
+      {
+        source: '/robots.txt',
+        destination: '/robots.txt',
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
